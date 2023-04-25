eb8efa0578f6c530d395ca69d2336ce44d6e6d79
AnilMallembakam@wireco.com <AnilMallembakam@wireco.com>
2023-04-25 19:54:07 +0000

    Added Staging ,Dim and  facts tables to models

--- models/src/src_reviews.sql
+++ models/src/src_reviews.sql
@@ -2,7 +2,7 @@ with raw_reviews as (select * from training_db.raw.raw_reviews)
 
 select
     listing_id,
-    date as revie_date,
+    date as review_date,
     reviewer_name,
     comments as review_text,
     sentiment as review_sentiment
